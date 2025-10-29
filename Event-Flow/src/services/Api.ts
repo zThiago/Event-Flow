d80990a90b4195e9f0c6f4ccb767197f8348bd5a
Thiago Ferreira <cunhathiagoferreira@gmail.com>
2025-10-28 23:59:07 -0300

    Api Service Frontend

---
+++ Event-Flow/src/services/Api.ts
@@ -0,0 +1,10 @@
+
+
+/**
+@function CreateUser - Cria um novo usuário na aplicação
+@params {Nome: string, Email: string, Senha: string} 
+@returns boolean - Se foi criado ou não
+*/
+export function CreateUser(DadosCadastrais : {Nome: string, Email: string, Senha: string}){
+    
+}
