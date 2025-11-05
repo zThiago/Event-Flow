e248bba8e307e16ff76842c5461f3fe0c35bdb44
Thiago Ferreira <cunhathiagoferreira@gmail.com>
2025-11-05 00:36:55 -0300

    Inicialização do projeto com o Swagger para documentação e configuração para segurança (Cors)

--- back-eventflow/src/main.ts
+++ back-eventflow/src/main.ts
@@ -1,16 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import {PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };	
-
-const prisma = globalForPrisma.prisma || new PrismaClient();
-if(process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  
+  const config = new DocumentBuilder()
+  .setTitle('Event Flow API')
+  .setDescription('Api para o aplicativo mobile Event Flow')
+  .setVersion('0.1')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
-export default prisma
