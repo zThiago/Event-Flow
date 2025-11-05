6d025fe362a4740cd5379ad849f650757d1121ac
Thiago Ferreira <cunhathiagoferreira@gmail.com>
2025-11-05 01:14:34 -0300

    Verificador de permissão do usuário

---
+++ back-eventflow/src/common/guards/roles.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+
+Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(private reflector: Reflector) {}
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.get<string[]>('roles', context.getHandler());
+        if(!requiredRoles) return true
+        const { user } = context.switchToHttp().getRequest();
+        return requiredRoles.includes(user.role);
+    }
+}
