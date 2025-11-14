6b317262a9ea1fae0eac38aa45e141fc2ca245af
Thiago Ferreira <cunhathiagoferreira@gmail.com>
2025-11-14 04:00:01 -0300

    feat: parametros no swagger feat: correção no dto do login e cadastro

--- back-eventflow/src/auth/dto/login.dto.ts
+++ back-eventflow/src/auth/dto/login.dto.ts
@@ -1,6 +1,8 @@
 import { IsEmail, IsString, Min, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+    @ApiProperty({example: 'usuario@email.com', description: 'Email do usuário'})
     @IsEmail()
     email: string;
 
@@ -10,14 +12,22 @@ export class LoginDto {
 }
 
 export class RegistroDto {
+    @ApiProperty({example: 'usuario@email.com', description: 'Email do usuário'})
     @IsEmail()
     email: string;
 
+    @ApiProperty({example: 'Senha123@', description: 'Senha do usuário, deve conter no minimo 6 caracteres'})
     @IsString()
     @MinLength(6)
     password: string;
 
+    @ApiProperty({example: 'João', description: 'Nome do usuário'})
     @IsString()
     @MinLength(2)
-    name: string;
+    nome: string;
+
+    @ApiProperty({example: '(11) 8402-8922', description: 'Telefone do usuário'})
+    @IsString()
+    @MinLength(10)
+    telefone: string;
 }
